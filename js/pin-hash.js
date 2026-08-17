88869989fbc1cd75b0004393acecc73b61324109
Baloo8721 <Baloo8721@users.noreply.github.com>
2026-08-17 02:14:57 +0000

    deploy: ec2330fd9a7b80774aff0c7f5371d240e7def634

--- js/pin-hash.js
+++ js/pin-hash.js
@@ -1,5 +1,5 @@
 // Generated — do not edit. Run: npm run build
 window.APP_CONFIG = {
-  "pinHash": "ae8755307889959622b9d1180d5288b253415903481b86118fdc3dd54275cd25",
-  "salt": "802007bbec9bc3b9b4cf3033b7edc44c"
+  "pinHash": "38f1b1198f54fc91c7fb8a025a2bb106c15f3304a4458c0bb446c877d778e4e3",
+  "salt": "5049553ba06747d38b6c5e8491a92c92"
 };
